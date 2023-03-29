e193dc7b10c4a3366a9638e7bbe6ea38e1cdf1bc
terlenev.dk <flaserhost@gmail.com>
2023-03-29 13:54:54 +0300

    business functions proceed

--- src/js/business-functions/general.js
+++ src/js/business-functions/general.js
@@ -41,3 +41,8 @@ document.addEventListener('click', e => {
         document.querySelector('button[title="Close"]').click();
     }
 });
+
+// перемещение к секции при входе на страницу
+
+const sectionPosition = document.querySelector('.main-content__page-destination').getBoundingClientRect().top;
+document.querySelector('html').scroll({top: sectionPosition, behavior: 'smooth'});
